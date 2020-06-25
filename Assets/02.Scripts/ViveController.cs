ab1ab16f9531e6e8be51ddb35fa46eb2924d1828
yoon9472 <57215037+yoon9472@users.noreply.github.com>
2020-06-25 15:50:41 +0900

    haptic

--- Assets/02.Scripts/ViveController.cs
+++ Assets/02.Scripts/ViveController.cs
@@ -16,6 +16,7 @@ public class ViveController : MonoBehaviour
     public SteamVR_Action_Boolean trackPadClick = SteamVR_Actions.default_Teleport;
     public SteamVR_Action_Boolean trackPadTouch = SteamVR_Actions.default_TrackPadTouch;
     public SteamVR_Action_Vector2 trackPadPosition = SteamVR_Actions.default_TrackPadPosition;
+    private SteamVR_Action_Vibration haptic = SteamVR_Actions.default_Haptic;
     void Awake()
     {
         trigger=  SteamVR_Actions.default_InteractUI;
@@ -25,6 +26,8 @@ public class ViveController : MonoBehaviour
         //왼손컨트롤
         if(SteamVR_Actions.default_InteractUI.GetStateDown(leftHand))
         {
+            Debug.Log("Clicked Trigger");
+            haptic.Execute(0.2f,0.3f,120.0f,0.5f,leftHand);
         }  
         //오른손컨트롤
         if(trigger.GetStateUp(rightHand))
